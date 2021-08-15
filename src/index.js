b97488ea7fb3d61f300953d2ba533b107336c7f1
EmilyRight <emilya.korkunova@gmail.com>
2021-08-16 00:00:49 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function toReadable (number) {
-  
+    let myStr = number.toString();
+
+  let units = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+  let dozens = [" ", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+  if (number >= 0 && number <= 19) {
+    return units[number];
+   
+    } else if (myStr.length == 2 && number%10 == 0){
+         return dozens[number/10];
+    } else if (myStr.length == 2 && number%10 > 0) {
+        return (dozens[+myStr[0]] + " " + units[+myStr[1]]).trim();
+    
+    } else if (myStr.length == 3 && number % 10 == 0){
+        return (units[+myStr[0]] + " hundred " + dozens[+myStr[1]]).trim();
+    } else if (myStr.length == 3 && !number % 100){
+        return (units[+myStr[0]] + " hundred").trim();
+    } else if (myStr.length == 3 && number % 100 > 0 && number % 100 < 20){
+        return (units[+myStr[0]]+ " hundred " + units[number%100]).trim();
+    
+    } else if (myStr.length == 3 && number % 100 > 19){
+        return (units[+myStr[0]]+ " hundred " + dozens[+myStr[1]] + " " + units[+myStr[2]]).trim(); 
+    }
 }
